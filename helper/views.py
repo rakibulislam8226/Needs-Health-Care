5204fecca2231fff2638da1fa4d04501341d68fb
rakibulislam8226 <rakibulislam8226@gmail.com>
2022-11-13 19:02:25 +0600

    frontend added

--- helper/views.py
+++ helper/views.py
@@ -4,29 +4,29 @@ from . models import Get_touch,CreateQuery
 
 # Create your views here.
 def home(request):
-    if request.method == "POST":
-        name = request.POST['name']
-        email = request.POST['email']
-        subject = request.POST['subject']
-        message = request.POST['message']
+    # if request.method == "POST":
+    #     name = request.POST['name']
+    #     email = request.POST['email']
+    #     subject = request.POST['subject']
+    #     message = request.POST['message']
 
-        obj = Get_touch(name=name, subject=subject, email=email, message=message)
-        obj.save()
+    #     obj = Get_touch(name=name, subject=subject, email=email, message=message)
+    #     obj.save()
 
-    if request.method == "POST":
-        qname = request.POST['qname']
-        qemail = request.POST['qemail']
-        qphone = request.POST['qphone']
-        qdate = request.POST['qdate']
-        qdepartment = request.POST['qdepartment']
-        qdescribe = request.POST['qdescribe']
+    # if request.method == "POST":
+    #     qname = request.POST['qname']
+    #     qemail = request.POST['qemail']
+    #     qphone = request.POST['qphone']
+    #     qdate = request.POST['qdate']
+    #     qdepartment = request.POST['qdepartment']
+    #     qdescribe = request.POST['qdescribe']
 
-        obj = CreateQuery(qname=qname, qemail=qemail, qphone=qphone, qdate=qdate,qdepartment=qdepartment,qdescribe=qdescribe)
-        obj.save()
+    #     obj = CreateQuery(qname=qname, qemail=qemail, qphone=qphone, qdate=qdate,qdepartment=qdepartment,qdescribe=qdescribe)
+    #     obj.save()
              
-    context={
-    }
-    return render(request, 'index.html')
+    # context={
+    # }
+    return render(request, 'base/index.html')
 
 
 def shop(request):
