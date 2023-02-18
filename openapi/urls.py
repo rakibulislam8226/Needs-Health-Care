b95117e8df8715c831378089755b9972a27ad8a7
rakibulislam8226 <rakibulislam8226@gmail.com>
2023-02-18 21:37:20 +0600

    modified openapi

--- openapi/urls.py
+++ openapi/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-# here we are importing all the Views from the views.py file
 from . import views
 
 # a list of all the urls
--- querys/models.py
+++ querys/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     post_title = models.CharField(max_length=255)
     descriptions = models.TextField()
-    answer_post = models.ForeignKey('AnswerPost', on_delete=models.CASCADE, null=True, blank=True, related_name='answer')
+    # answer_post = models.ForeignKey('AnswerPost', on_delete=models.CASCADE, null=True, blank=True, related_name='answer')
 
     class Meta:
         ordering = ['-id']
@@ -23,7 +23,7 @@ class Post(models.Model):
 class AnswerPost(models.Model):
     name = models.CharField(max_length=20)
     answer_text = models.TextField()
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
      
     def __str__(self):
         return f"Answer by Name: {self.name} test {self.answer_text}"
